3fb9465e739917aa7ff37921886e2ecf77c46e9d
Alex <al.linard@gmail.com>
2020-04-08 13:59:42 +0200

    refactoring signal generation

--- STLpulp.py
+++ STLpulp.py
@@ -2,24 +2,35 @@ from STL import STLFormula
 import operator as operatorclass
 import pulp as plp
 import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import matplotlib.patches as patches
 import numpy as np
 import random
 
 
 #CONSTANTS
-INDEX_X = 0
-INDEX_Y = 1
-M = 1000
+M = 100000
 M_up = 100000
 M_low = 0.000001
 
+#HARDCODED
+#TODO: manage more dimensions
+NB_DIMENSIONS = 2
 
-#Control policy, i.e. how much can move in 1 time stamp
-U = 0.2
 
 
-def generate_trajectory_milp(phi,start,rand_area,OPTIMIZE_ROBUSTNESS):
-    
+def generate_signal_milp_quantitative(phi,start,rand_area,U,epsilon,OPTIMIZE_ROBUSTNESS):
+    """
+        Function generating a signal satisfying an STL Formula.
+        Takes as input:
+            * phi: an STL Formula
+            * start: a vector of the form [x0,y0] for the starting point coordinates
+            * rand_area: the domain on which signals are generated. rand_area = [lb,ub] where lb is the lower bound and ub the upper bound of the domain.
+            * U: a basic control policy standing for how much can move in 1 time stamp, i.e. \forall t \in [0,T], |s[t]-s[t+1]| < U \pm \epsilon 
+            * epsilon: basic control policy parameter
+            * OPTIMIZE_ROBUSTNESS: a flag whether the robustness of the generated signal w.r.t. phi has to be maximized or not
+        The encoding details of the MILP optimization problem follows the quantitative enconding of Raman et al., "Model  predictive  control  with  signaltemporal logic specifications" in 53rd IEEE Conference on Decision and Control. IEEE, 2014, pp. 81–87.
+    """    
     dict_vars = {}
   
     #objective, maximize robustness
@@ -35,18 +46,19 @@ def generate_trajectory_milp(phi,start,rand_area,OPTIMIZE_ROBUSTNESS):
     
 
     #We want to optimize a signal. The lower and upperbounds are specified by the random area.
-    s = plp.LpVariable.dicts("s",(range(phi.horizon+1),range(2)),rand_area[0],rand_area[1],plp.LpContinuous)
+    s = plp.LpVariable.dicts("s",(range(phi.horizon+1),range(NB_DIMENSIONS)),rand_area[0],rand_area[1],plp.LpContinuous)
 
     #the start is specified
     opt_model += s[0][0] == start[0]
     opt_model += s[0][1] == start[1]
     
-    #control policy
+    #basic control policy, i.e. how much can move in 1 time stamp
+    #\forall t \in [0,T], |s[t]-s[t+1]| < U \pm \epsilon 
     for t in range(0,phi.horizon):
-        opt_model += s[t+1][0]-s[t][0] <= random.uniform(U-0.1,U+0.1)
-        opt_model += -(s[t+1][0]-s[t][0]) <= random.uniform(U-0.1,U+0.1)
-        opt_model += s[t+1][1]-s[t][1] <= random.uniform(U-0.1,U+0.1)
-        opt_model += -(s[t+1][1]-s[t][1]) <= random.uniform(U-0.1,U+0.1)
+        opt_model += s[t+1][0]-s[t][0] <= random.uniform(U-epsilon,U+epsilon)
+        opt_model += -(s[t+1][0]-s[t][0]) <= random.uniform(U-epsilon,U+epsilon)
+        opt_model += s[t+1][1]-s[t][1] <= random.uniform(U-epsilon,U+epsilon)
+        opt_model += -(s[t+1][1]-s[t][1]) <= random.uniform(U-epsilon,U+epsilon)
         
         
     #recursive function
@@ -171,23 +183,31 @@ def generate_trajectory_milp(phi,start,rand_area,OPTIMIZE_ROBUSTNESS):
     rvar = dict_vars['r_'+str(id(phi))+'_t_'+str(phi.horizon)]
     opt_model += rvar >= 0 
     
-    # opt_model.solve()
     opt_model.solve(plp.GUROBI_CMD(msg=False))
 
-    # return np.array([[s[j][i].varValue for i in range(2)] for j in range(phi.horizon+1)])
     if s[0][0].varValue == None:
         raise Exception("")
     
-    return [[s[j][i].varValue for i in range(2)] for j in range(phi.horizon+1)]
+    return [[s[j][i].varValue for i in range(NB_DIMENSIONS)] for j in range(phi.horizon+1)]
     
     
     
 
 
-def generate_trajectory_boolean_milp(phi,start,rand_area):
+def generate_signal_milp_boolean(phi,start,rand_area,U,epsilon):
+    """
+        Function generating a signal satisfying an STL Formula.
+        Takes as input:
+            * phi: an STL Formula
+            * start: a vector of the form [x0,y0] for the starting point coordinates
+            * rand_area: the domain on which signals are generated. rand_area = [lb,ub] where lb is the lower bound and ub the upper bound of the domain.
+            * U: a basic control policy standing for how much can move in 1 time stamp, i.e. \forall t \in [0,T], |s[t]-s[t+1]| < U \pm \epsilon 
+            * epsilon: basic control policy parameter
+        The encoding details of the MILP optimization problem follows the boolean enconding of Raman et al., "Model  predictive  control  with  signaltemporal logic specifications" in 53rd IEEE Conference on Decision and Control. IEEE, 2014, pp. 81–87.
+    """    
     dict_vars = {}
   
-    #satisfaction of phi1 and phi2
+    #satisfaction of phi
     zvar1 = plp.LpVariable('z1_'+str(id(phi))+'_t_'+str(phi.horizon),cat='Binary')
     dict_vars['z1_'+str(id(phi))+'_t_'+str(phi.horizon)] = zvar1
 
@@ -195,7 +215,7 @@ def generate_trajectory_boolean_milp(phi,start,rand_area):
   
             
     #We want to optimize a signal. The lower and upperbounds are specified by the random area.
-    s = plp.LpVariable.dicts("s",(range(phi.horizon+1),range(2)),rand_area[0],rand_area[1],plp.LpContinuous)
+    s = plp.LpVariable.dicts("s",(range(phi.horizon+1),range(NB_DIMENSIONS)),rand_area[0],rand_area[1],plp.LpContinuous)
        
     #the start is specified
     opt_model += s[0][0] == start[0]
@@ -203,10 +223,10 @@ def generate_trajectory_boolean_milp(phi,start,rand_area):
     
     #control policy
     for t in range(0,phi.horizon):
-        opt_model += s[t+1][0]-s[t][0] <= random.uniform(U-0.1,U+0.1)
-        opt_model += -(s[t+1][0]-s[t][0]) <= random.uniform(U-0.1,U+0.1)
-        opt_model += s[t+1][1]-s[t][1] <= random.uniform(U-0.1,U+0.1)
-        opt_model += -(s[t+1][1]-s[t][1]) <= random.uniform(U-0.1,U+0.1)   
+        opt_model += s[t+1][0]-s[t][0] <= random.uniform(U-epsilon,U+epsilon)
+        opt_model += -(s[t+1][0]-s[t][0]) <= random.uniform(U-epsilon,U+epsilon)
+        opt_model += s[t+1][1]-s[t][1] <= random.uniform(U-epsilon,U+epsilon)
+        opt_model += -(s[t+1][1]-s[t][1]) <= random.uniform(U-epsilon,U+epsilon)   
        
     #recursive function
     def model_phi1(phi,t,opt_model):
@@ -229,6 +249,19 @@ def generate_trajectory_boolean_milp(phi,start,rand_area):
             else:
                 opt_model += -s[t][phi.pi_index_signal] + phi.mu <= M_up*zvar-M_low
                 opt_model += -(-s[t][phi.pi_index_signal] + phi.mu) <= M_up*(1-zvar)-M_low
+        elif isinstance(phi, STLFormula.Negation):
+            model_phi1(phi.formula,t,opt_model)
+            try:
+                zvar1 = dict_vars['z1_'+str(id(phi.formula))+'_t_'+str(t)]
+            except KeyError:
+                zvar1 = plp.LpVariable('z1_'+str(id(phi.formula))+'_t_'+str(t),cat='Binary')
+                dict_vars['z1_'+str(id(phi.formula))+'_t_'+str(t)] = zvar1 
+            try:
+                zvar = dict_vars['z1_'+str(id(phi))+'_t_'+str(t)]
+            except KeyError:
+                zvar = plp.LpVariable('z1_'+str(id(phi))+'_t_'+str(t),cat='Binary')
+                dict_vars['z1_'+str(id(phi))+'_t_'+str(t)] = zvar
+            opt_model += zvar == 1-zvar1
         elif isinstance(phi, STLFormula.Conjunction):
             model_phi1(phi.first_formula,t,opt_model)
             model_phi1(phi.second_formula,t,opt_model)
@@ -308,66 +341,105 @@ def generate_trajectory_boolean_milp(phi,start,rand_area):
         
     opt_model.solve(plp.GUROBI_CMD(msg=False))
     
-    return [[s[j][i].varValue for i in range(2)] for j in range(phi.horizon+1)]
+    return [[s[j][i].varValue for i in range(NB_DIMENSIONS)] for j in range(phi.horizon+1)]
 
     
     
     
     
-def test():
-    #test metrics
-    predicate_x_ge02 = STLFormula.Predicate('x',operatorclass.ge,0.2,INDEX_X)
-    predicate_x_lt04 = STLFormula.Predicate('x',operatorclass.le,0.4,INDEX_X)
-    predicate_x_lt044 = STLFormula.Predicate('x',operatorclass.le,0.44,INDEX_X)
+if __name__ == '__main__':
 
-    phi1 = STLFormula.Always(STLFormula.Conjunction(predicate_x_ge02,predicate_x_lt04),0,20)
-    phi2 = STLFormula.Always(STLFormula.Conjunction(predicate_x_ge02,predicate_x_lt044),0,20)
-    phi3 = STLFormula.Eventually(STLFormula.Conjunction(predicate_x_ge02,predicate_x_lt04),0,20)
-    phi4 = STLFormula.Conjunction(STLFormula.Always(STLFormula.Conjunction(predicate_x_ge02,predicate_x_lt04),0,20),STLFormula.Eventually(STLFormula.Conjunction(predicate_x_ge02,predicate_x_lt044),0,20))
-    phi5 = STLFormula.Conjunction(STLFormula.Always(STLFormula.Conjunction(predicate_x_ge02,predicate_x_lt04),0,10),STLFormula.Always(STLFormula.Conjunction(predicate_x_ge02,predicate_x_lt044),12,20))
-    phi6 = STLFormula.Always(STLFormula.Eventually(STLFormula.Conjunction(predicate_x_ge02,predicate_x_lt04),0,4),0,16)
+    #CONSTANTS
+    INDEX_X = 0
+    INDEX_Y = 1
 
-
-    phi8 = STLFormula.Always(STLFormula.Disjunction(STLFormula.Predicate('x',operatorclass.ge,0.4,INDEX_X),STLFormula.Predicate('y',operatorclass.le,0.2,INDEX_X)),0,20)
-
-
-
+    #Definition of STL Formulae
+    predicate_x_gt0 = STLFormula.Predicate('x',operatorclass.gt,0,INDEX_X)
+    predicate_x_le1 = STLFormula.Predicate('x',operatorclass.le,1,INDEX_X)
+    predicate_y_gt3 = STLFormula.Predicate('y',operatorclass.gt,3,INDEX_Y)
+    predicate_y_le4 = STLFormula.Predicate('y',operatorclass.le,4,INDEX_Y)
+    eventually1 = STLFormula.Eventually(STLFormula.Conjunction( STLFormula.Conjunction(predicate_x_gt0,predicate_x_le1) , STLFormula.Conjunction(predicate_y_gt3,predicate_y_le4) ),10,20)
     predicate_x_gt3 = STLFormula.Predicate('x',operatorclass.gt,3,INDEX_X)
     predicate_x_le4 = STLFormula.Predicate('x',operatorclass.le,4,INDEX_X)
-    predicate_y_gt2 = STLFormula.Predicate('y',operatorclass.gt,2,INDEX_Y)
-    predicate_y_le3 = STLFormula.Predicate('y',operatorclass.le,3,INDEX_Y)
-    eventually = STLFormula.Eventually(STLFormula.Conjunction( STLFormula.Conjunction(predicate_x_gt3,predicate_x_le4) , STLFormula.Conjunction(predicate_y_gt2,predicate_y_le3) ),0,30)
-    predicate_x_gt1 = STLFormula.Predicate('x',operatorclass.gt,1,INDEX_X)
-    predicate_x_le2 = STLFormula.Predicate('x',operatorclass.le,2,INDEX_X)
-    always = STLFormula.Always( STLFormula.Negation( STLFormula.Conjunction( STLFormula.Conjunction(predicate_x_gt1,predicate_x_le2) , STLFormula.Conjunction(predicate_y_gt2,predicate_y_le3) ) ),0,30) 
-    phi = STLFormula.Conjunction(eventually,always)
-    phi2 = STLFormula.Conjunction(phi,STLFormula.Always( STLFormula.Negation( STLFormula.Conjunction( STLFormula.Conjunction(STLFormula.Predicate('x',operatorclass.gt,1,INDEX_X),STLFormula.Predicate('x',operatorclass.lt,2,INDEX_X)) , STLFormula.Conjunction(STLFormula.Predicate('y',operatorclass.gt,0.5,INDEX_Y),STLFormula.Predicate('y',operatorclass.lt,1.5,INDEX_Y)) ) ),0,30) )
-    phi_nnf = STLFormula.toNegationNormalForm(phi2,False)
+    predicate_y_gt0 = STLFormula.Predicate('y',operatorclass.gt,0,INDEX_Y)
+    predicate_y_le1 = STLFormula.Predicate('y',operatorclass.le,1,INDEX_Y)
+    eventually2 = STLFormula.Eventually(STLFormula.Conjunction( STLFormula.Conjunction(predicate_x_gt3,predicate_x_le4) , STLFormula.Conjunction(predicate_y_gt0,predicate_y_le1) ),30,50)
+    predicate_x_gt2 = STLFormula.Predicate('x',operatorclass.gt,2,INDEX_X)
+    predicate_y_gt1 = STLFormula.Predicate('y',operatorclass.gt,1,INDEX_Y)
+    predicate_x_le3 = STLFormula.Predicate('x',operatorclass.le,3,INDEX_X)
+    predicate_y_le2 = STLFormula.Predicate('y',operatorclass.le,2,INDEX_Y)
+    always = STLFormula.Always( STLFormula.Negation( STLFormula.Conjunction( STLFormula.Conjunction(predicate_x_gt2,predicate_x_le3) , STLFormula.Conjunction(predicate_y_gt1,predicate_y_le2) ) ),0,50) 
+    phi = STLFormula.Conjunction(STLFormula.Conjunction(eventually1,eventually2),always)
+    phi_nnf = STLFormula.toNegationNormalForm(phi,False)
 
-
-
-    phi_test = STLFormula.Always(STLFormula.Conjunction(STLFormula.Conjunction(STLFormula.Predicate('x',operatorclass.ge,-0.5,INDEX_X),STLFormula.Predicate('x',operatorclass.le,2,INDEX_X)),STLFormula.Conjunction(STLFormula.Predicate('y',operatorclass.ge,-0.5,INDEX_Y),STLFormula.Predicate('y',operatorclass.le,0.8,INDEX_Y))),0,20)
-
-
-
+    #parameters
     start=[0, 0]
-    rand_area=[-2, 4]
+    rand_area=[0, 4]
+    U = 0.2
+    epsilon = 0.05
+    
+    #generation of 3 trajectories (quantitative no maximization, quantitative with maximization, boolean)
+    trajectory1 = generate_signal_milp_quantitative(phi_nnf,start,rand_area,U,epsilon,False)
+    trajectory2 = generate_signal_milp_quantitative(phi_nnf,start,rand_area,U,epsilon,True)
+    trajectory3 = generate_signal_milp_boolean(phi_nnf,start,rand_area,U,epsilon)
 
-
-    # trajectory = generate_trajectory_milp(phi_nnf,start,rand_area,True)
-    trajectory = generate_trajectory_boolean_milp(phi_nnf,start,rand_area)
-    print(trajectory)
-    # print(phi_nnf.tex())
-    x, y = np.array(trajectory).T
+    #Plot
+    plt.clf()
     fig = plt.figure()
-    ax = plt.subplot(111)
-    ax.scatter(x,y)
-    ax.plot(x, y, label=r'$\rho='+str(phi_nnf.robustness(trajectory,0))+'$')
-    plt.title(r"$"+phi_nnf.tex()+"$")
-    plt.ylim(rand_area[0], rand_area[1])
-    plt.xlim(rand_area[0], rand_area[1])
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),shadow=True, ncol=2)
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    fig.tight_layout()
+    codes = [Path.MOVETO,
+         Path.LINETO,
+         Path.LINETO,
+         Path.LINETO,
+         Path.CLOSEPOLY,
+         ]
+    verts4_1 = [
+        (0., 3.), # left, bottom
+        (0., 4.), # left, top
+        (1., 4.), # right, top
+        (1., 3.), # right, bottom
+        (0., 0.), # ignored
+        ]
+    path4_1 = Path(verts4_1, codes)
+    patch4_1 = patches.PathPatch(path4_1, facecolor='honeydew',lw=0)
+    verts4_2 = [
+        (2., 1.), # left, bottom
+        (2., 2.), # left, top
+        (3., 2.), # right, top
+        (3., 1.), # right, bottom
+        (0., 0.), # ignored
+        ]
+    path4_2 = Path(verts4_2, codes)
+    patch4_2 = patches.PathPatch(path4_2, facecolor='mistyrose',lw=0)
+    verts4_3 = [
+        (3., 0.), # left, bottom
+        (3., 1.), # left, top
+        (4., 1.), # right, top
+        (4., 0.), # right, bottom
+        (0., 0.), # ignored
+        ]
+    path4_3 = Path(verts4_3, codes)
+    patch4_3 = patches.PathPatch(path4_3, facecolor='honeydew',lw=0)
+    ax.add_patch(patch4_1)
+    ax.add_patch(patch4_2)
+    ax.add_patch(patch4_3)
+    plt.gcf().canvas.mpl_connect('key_release_event',
+                                 lambda event: [exit(0) if event.key == 'escape' else None])
+    plt.axis([rand_area[0]-0.2, rand_area[1]+0.2, rand_area[0]-0.2, rand_area[1]+0.2])
+    plt.grid(True)
+
+    ax.plot([x for (x, y) in trajectory1], [y for (x, y) in trajectory1], '-g', marker='o', label=r'quantitave $\rho='+str(round(phi.robustness(trajectory1,0),3))+'$')
+    plt.grid(True)
+
+    ax.plot([x for (x, y) in trajectory2],[y for (x, y) in trajectory2], '-b', marker='o', label=r'quantitave optimized $\rho='+str(round(phi.robustness(trajectory2,0),3))+'$')
+    plt.grid(True)     
+    
+    ax.plot([x for (x, y) in trajectory3],[y for (x, y) in trajectory3], '-r', marker='o', label=r'boolean $\rho='+str(round(phi.robustness(trajectory3,0),3))+'$')
+    plt.grid(True)                    
+
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),shadow=True, ncol=3)
     plt.show()
-    
-    
-# test()
+
